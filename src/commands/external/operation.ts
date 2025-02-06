68b95eeb8908359fd87877eb6ef6403086089b12
Muhammad Aaqil <aaqilcs102@gmail.com>
2025-02-06 19:30:47 +0500

    fix: external:operation
    Signed-off-by: Muhammad Aaqil <aaqilcs102@gmail.com>

--- src/commands/external/operation.ts
+++ src/commands/external/operation.ts
@@ -3,7 +3,7 @@ import chalk from 'chalk';
 import fs from 'fs';
 
 import { processOptions, toPascalCase, toKebabCase, execute, addImport } from '../../utils/index.js';
-import { Project, SyntaxKind, PropertyAssignment, ObjectLiteralExpression, ArrayLiteralExpression, OptionalKind, ParameterDeclarationStructure } from 'ts-morph';
+import { Project, SyntaxKind, PropertyAssignment, ObjectLiteralExpression, ArrayLiteralExpression, OptionalKind, ParameterDeclarationStructure, EnumMember } from 'ts-morph';
 
 export default class ExternalOperation extends Command {
 
@@ -12,18 +12,19 @@ export default class ExternalOperation extends Command {
   static override flags = {
     config: Flags.string({ char: 'c', description: 'Config JSON object' }),
     ds: Flags.string({ description: 'datasource name to attach APIs to.' }),
-    method: Flags.string({ description: 'api method type.' }),
-    bodyParams: Flags.string({ description: 'Stringified JSON Object for request body.' }),
-    pathParams: Flags.string({ description: 'Stringified JSON Object for path parameter.' }),
-    url: Flags.string({ description: 'url of the external api.' }),
-    headers: Flags.string({ description: 'Stringified JSON Object for headers.' }),
-    query: Flags.string({ description: 'Stringified JSON Object for query parameters.' }),
-    apiFunction: Flags.string({ description: 'api function name.' }),
-    name: Flags.string({ description: 'name of the artifacts.' }),
-    apiUri: Flags.string({ description: 'uri of the controller.' }),
-    type: Flags.string({ description: 'type of return data.' }),
-    returnModel: Flags.string({ description: 'name of the model in success responses.' }),
     controller: Flags.string({ description: 'controller under which the API should reside.' }),
+    method: Flags.string({ description: 'api method type.' }),
+    url: Flags.string({ description: 'url of the external api.' }),
+    bodyParams: Flags.string({ description: 'Stringified JSON Object for request body.' }),
+    createModel: Flags.boolean({ description: 'generate the model.' }),
+    additionalProperties: Flags.string({ description: 'stringified additional model properties.' }),
+    apiFunction: Flags.string({ description: 'api function name.' }),
+    pathParams: Flags.string({ description: 'Stringified JSON Object for path parameter.' }),
+    queryParams: Flags.string({ description: 'Stringified JSON Object for query parameters.' }),
+    headers: Flags.string({ description: 'Stringified JSON Object for headers.' }),
+    apiUri: Flags.string({ description: 'uri of the controller.' }),
+    responses: Flags.string({ description: 'Stringified JSON Object for responses.' }),
+    description: Flags.string({ description: 'Description of request.' }),
   }
 
   public async run(): Promise<void> {
@@ -37,79 +38,37 @@ export default class ExternalOperation extends Command {
       const {
         ds,
         url,
-        headers,
+        createModel,
+        additionalProperties,
         apiFunction,
-        type,
-        controller,
+        headers,
       } = options;
       let {
         method,
+        controller,
+        modelName,
+        requestModelName,
         pathParams,
         bodyParams,
         queryParams,
-        name,
         apiUri,
+        responses,
         spliter,
-        returnModel
+        description,
       } = options;
 
-      if (!spliter && !apiUri) spliter = 'v1';
-      if (spliter) apiUri = url.split(spliter)[1];
-
-      if (!pathParams) pathParams = {};
-
-      let functionName = name;
-      let artifactName = functionName;
-
-      const pathKey = Object.keys(pathParams)[0];
-
-      let queryParamList: string[] = [];
-      let bodyParamList: string[] = [];
-      let serviceName = toPascalCase(controller);
-      let modelName = toPascalCase(artifactName);
-      let controllerName = toPascalCase(controller);
-      let description = '';
-
       if (!method) method = 'get';
-      switch (method.toLowerCase()) {
-        case 'get':
-          description = `fetch${pathKey ? '' : ' all'} ${toPascalCase(functionName)}.`
-          break;
-        case 'post':
-          description = `create ${toPascalCase(functionName)}.`
-          break;
-        case 'patch':
-          description = `update ${toPascalCase(functionName)}.`
-          break;
-        case 'put':
-          description = `replace ${toPascalCase(functionName)}.`
-          break;
-        case 'del':
-          description = `delete ${toPascalCase(functionName)}`;
-          break;
-        default:
-          break;
-      }
-      if (!queryParams) { queryParams = []; }
-      queryParams.forEach((eachParam: any) => {
-        Object.keys(eachParam).forEach(key => { queryParamList.push(key); });
-      });
-      if (bodyParams) {
-        bodyParams.forEach((eachParam: any) => {
-          Object.keys(eachParam).forEach(key => { bodyParamList.push(key); });
-        });
-      }
+      method = method.toLowerCase();
 
       const project = new Project({});
       const invokedFrom = process.cwd();
-
       project.addSourceFilesAtPaths(`${invokedFrom}/src/**/*.ts`);
-      const restBasedDS = fs.existsSync(`./src/datasources/${ds}.datasource.ts`);
 
-      if (!restBasedDS) {
-        throw new Error(`the ${ds} doesn't exist.`);
-      }
       const dsPath = `${invokedFrom}/src/datasources/${ds}.datasource.ts`;
+
+      const restBasedDS = fs.existsSync(dsPath);
+      if (!restBasedDS) throw new Error(`the ${ds} doesn't exist.`);
+
       const dsFile = project.getSourceFile(dsPath);
       const configVariable = dsFile?.getVariableDeclarationOrThrow('config')!;
       const initializer = configVariable.getInitializerIfKindOrThrow(SyntaxKind.ObjectLiteralExpression) as ObjectLiteralExpression;
@@ -132,22 +91,64 @@ export default class ExternalOperation extends Command {
         throw new Error(`operation ${method} ${url} already exists.`);
       }
 
+      if (!spliter && !apiUri) spliter = 'v1';
+      if (spliter) apiUri = url.split(spliter)[1];
+
+      if (!pathParams) pathParams = {};
+
+      const pathKey = Object.keys(pathParams)[0];
+      let queryParamList: string[] = [];
+      let bodyParamList: string[] = [];
+      let serviceName = toPascalCase(controller);
+
+      modelName = toPascalCase(modelName);
+      controller = toPascalCase(controller);
+
+      if (!description) {
+
+        switch (method) {
+          case 'get':
+            description = `fetch${pathKey ? '' : ' all'} ${toPascalCase(modelName)}.`
+            break;
+          case 'post':
+            description = `create ${toPascalCase(modelName)}.`
+            break;
+          case 'patch':
+            description = `update ${toPascalCase(modelName)}.`
+            break;
+          case 'put':
+            description = `replace ${toPascalCase(modelName)}.`
+            break;
+          case 'del':
+            description = `delete ${toPascalCase(modelName)}`;
+            break;
+          default:
+            break;
+        }
+      }
+
+      if (!queryParams) queryParams = {}; // prevents checking existing queryParams in the future too.
+      Object.keys(queryParams).forEach(key => { queryParamList.push(key) })
+
+      if (!bodyParams) bodyParams = { properties: {} }; // prevents checking existing queryParams in the future too.
+      Object.keys(bodyParams.properties).forEach((key) => { bodyParamList.push(key); });
+
       let query = '';
       let body = '';
       let path = '';
       let functionParams = '';
 
-      queryParamList.forEach((key: string) => { query += `${key}: '{${key}}', `; });
-      bodyParamList.forEach((key: string) => { body += `${key}: '{${key}}', `; });
+      queryParamList.forEach((key: string) => {
+        query += `${key}: '{${key}}', `;
+        functionParams += `'${key}',`;
+      });
+      bodyParamList.forEach((key: string) => {
+        body += `${key}: '{${key}}', `;
+        functionParams += `'${key}',`;
+      });
+      Object.keys(pathParams).forEach((key: string) => { functionParams += `'${key}',`; });
 
       if (pathKey) path = `${pathKey}: '{${pathKey}}'`;
-
-      queryParamList.forEach((key: string) => { functionParams += `'${key}',`; });
-
-      if (bodyParamList.length) {
-        bodyParamList.forEach((key: string) => { functionParams += `'${key}',`; });
-      }
-      Object.keys(pathParams).forEach((key: string) => { functionParams += `'${key}',`; });
 
       let operation = `{
         template: {
@@ -173,61 +174,72 @@ export default class ExternalOperation extends Command {
       if (pathKey) {
         parameters.push({
           name: pathKey,
-          type: pathParams[pathKey],
+          type: pathParams[pathKey]['type'],
           decorators: [{
-            name: `param.path.${pathParams[pathKey]}`,
+            name: `param.path.${pathParams[pathKey]['type']}`,
             arguments: [`'${pathKey}'`]
           }]
         })
       }
-      queryParams.forEach((param: any) => {
-        Object.keys(param).forEach(key => {
-          parameters.push({
-            name: key,
-            type: param[key],
-            decorators: [{
-              name: `param.query.${param[key]}`,
-              arguments: [`'${key}'`]
-            }]
-          });
-          serviceParams += `${key}: ${param[key]},`;
+      Object.keys(queryParams).forEach((key) => {
+        const type = queryParams[key]['type'];
+        parameters.push({
+          name: key,
+          type,
+          decorators: [{
+            name: `param.query.${type}`,
+            arguments: [`'${key}'`]
+          }]
         });
+        serviceParams += `${key}: ${type},`;
       });
 
-
-      const controllerFilePath = `${invokedFrom}/src/controllers/${toKebabCase(controllerName)}.controller.ts`;
+      const controllerFilePath = `${invokedFrom}/src/controllers/${toKebabCase(controller)}.controller.ts`;
       let controllerFile = project.getSourceFile(controllerFilePath);
-      const modelFileName = returnModel || modelName;
-      const model = fs.existsSync(`./src/models/${toKebabCase(modelFileName)}.model.ts`);
-      addImport(controllerFile, modelName, '../models', true);
+      const model = fs.existsSync(`./src/models/${toKebabCase(requestModelName || modelName)}.model.ts`);
+      if (model) addImport(controllerFile, requestModelName || modelName, '../models', true);
 
       if (body) {
+        let name = `requestBody({content: {'application/json': {schema: getModelSchemaRef(${requestModelName || modelName})}}})`;
+        if (bodyParams.type === 'array') {
+          name = `requestBody({content: {'application/json': {schema: {type: 'array', items: getModelSchemaRef(${requestModelName || modelName})}}}})`;
+        }
         const properties: { [x: string]: object } = {};
         const parameter = {
           name: 'body',
           type: 'any',
           decorators: [{
-            name: `requestBody({content: {'application/json': {schema: getModelSchemaRef(${modelName})}}})`,
+            name
           }]
         }
-        if (type) parameter.type = type;
 
         parameters.push(parameter);
 
-        if (!bodyParams) bodyParams = [];
+        Object.keys(bodyParams.properties).forEach((key: any) => {
+          const property: { [x: string]: string | object } = {};
+          property.type = bodyParams.properties[key]['type'];
+          property.required = bodyParams.properties[key]['required'] || false;
+          let type = property.type;
 
-        bodyParams.forEach((bodyParam: any) => {
-          Object.keys(bodyParam).forEach(key => {
-            properties[key] = { type: bodyParam[key] }
-            serviceParams += `${key}: ${bodyParam[key]} | undefined,`;
-          });
+          const enumValues = bodyParams.properties[key]['enum'];
+
+          if (enumValues && enumValues.length) {
+            property.jsonSchema = { enum: enumValues }
+          }
+          properties[key] = property;
+          if (!property.required) { type += ' | undefined'; }
+          serviceParams += `${key}: ${type},`;
         });
+        if (additionalProperties) {
+          Object.keys(additionalProperties).forEach(key => {
+            properties[key] = additionalProperties[key];
+          });
+        }
 
-
-        if (!model) {
+        if (!model && createModel) {
           const configs = {
             base: 'Entity',
-            name: modelName,
+            name: requestModelName || modelName,
             properties
           };
           const stringifiedConfigs = JSON.stringify(configs);
@@ -235,45 +247,60 @@ export default class ExternalOperation extends Command {
           let executed: any = await execute(command, 'generating models.');
           if (executed.stderr) console.log(chalk.bold(chalk.green(executed.stderr)));
           if (executed.stdout) console.log(chalk.bold(chalk.green(executed.stdout)));
+          addImport(controllerFile, requestModelName || modelName, '../models', true);
         }
 
       }
-
       if (pathKey) {
-        serviceParams += `${pathKey}: ${pathParams[pathKey]}`;
+        serviceParams += `${pathKey}: ${pathParams[pathKey]['type']}`;
       }
 
-      const existingController = fs.existsSync(`./src/controllers/${toKebabCase(controllerName)}.controller.ts`);
+      const existingController = fs.existsSync(`./src/controllers/${toKebabCase(controller)}.controller.ts`);
       if (!existingController) {
-        let command = `lb4 controller --config='{"name": "${controllerName}"}' --yes`;
+        let command = `lb4 controller --config='{"name": "${controller.toLowerCase()}"}' --yes`;
         let executed: any = await execute(command, 'generating controller.');
         if (executed.stderr) console.log(chalk.bold(chalk.green(executed.stderr)));
         if (executed.stdout) console.log(chalk.bold(chalk.green(executed.stdout)));
       }
 
-      project.addSourceFilesAtPaths(`${invokedFrom}/src/**/*.ts`);
-
       controllerFile = project.getSourceFile(controllerFilePath);
+
       if (controllerFile) {
-        addImport(controllerFile, `${(method).toLowerCase()}`, '@loopback/rest', true);
+        addImport(controllerFile, `${method}`, '@loopback/rest', true);
         addImport(controllerFile, `requestBody, getModelSchemaRef, param`, '@loopback/rest', true);
         addImport(controllerFile, 'inject', '@loopback/core');
         addImport(controllerFile, `${serviceName} as ${serviceName}Service`, '../services');
 
+        type ResponseType = {
+          description?: string;
+          content?: { [x: string]: object };
+        }
         // Find all class declarations within the source file
-        const classDeclaration = controllerFile?.getClass(`${toPascalCase(controllerName)}Controller`);
+        const classDeclaration = controllerFile?.getClass(`${toPascalCase(controller)}Controller`);
         const classConstructors = classDeclaration?.getConstructors() || [];
-        let apiMethod = classDeclaration?.getMethod(functionName.toLowerCase());
-        if (!returnModel) returnModel = modelName;
+        let apiMethod = classDeclaration?.getMethod(apiFunction);
+        let constructedResponses: { [x: string]: ResponseType } = {};
+        let finalResponses = {};
+        Object.keys(responses).forEach(responseCode => {
+          constructedResponses[responseCode] = {};
+          const response = responses[responseCode];
+          constructedResponses[responseCode]['description'] = response.description || description;
+          let schema: string | { [x: string]: string | object } = {};
+          schema.type = response.schema.type;
+          if (response.schema.model) {
+            if (schema.type === 'array') {
+              schema.items = `getModelSchemaRef(${modelName})`;
+            } else {
+              schema = `getModelSchemaRef(${modelName})`;
+            }
+          }
+          if (response.schema.properties) {
+            schema = { properties: response.schema.properties };
+          }
+          constructedResponses[responseCode]['content'] = { 'application/json': { schema } };
+          finalResponses = { responses: constructedResponses };
+        });
         if (!apiMethod) {
-          let responses = `{
-            responses: {
-              '200': {
-                description: '${description}',
-                content: {'application/json': {schema: ${model ? `{type: 'array', items: getModelSchemaRef(${returnModel})}` : '{}'}}},
-              },
-            },
-          }`;
           let methodParameters = queryParamList.toString();
           if (body) {
             if (
@@ -293,7 +320,8 @@ export default class ExternalOperation extends Command {
             }
             methodParameters += `${pathKey}`;
           }
-
+          let temp = JSON.stringify(finalResponses);
+          temp = temp.replaceAll(`"getModelSchemaRef(${modelName})"`, `getModelSchemaRef(${modelName})`)
           let methodStructure = {
             name: apiFunction,
             parameters,
@@ -302,15 +330,13 @@ export default class ExternalOperation extends Command {
             decorators: [
               {
                 name: (method).toLowerCase(),
-                arguments: [`'${apiUri}'`, responses]
+                arguments: [`'${apiUri}'`, temp]
               }
             ],
             returnType: `Promise<any>`
           }
-
           classDeclaration?.addMethod(methodStructure);
         }
-        apiMethod = classDeclaration?.getMethod(functionName.toLowerCase());
 
         if (!classConstructors[0].getParameter('service')) {
           classConstructors[0].insertParameter(0, {
@@ -325,6 +351,7 @@ export default class ExternalOperation extends Command {
           });
         }
       }
+
       controllerFile?.formatText();
       controllerFile?.saveSync();
 
